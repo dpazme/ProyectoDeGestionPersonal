2bd88ca02892572a122bc199bb799520ac256b13
damdepm <damdepm@gmail.com>
2024-10-08 10:34:25 +0200

    plantilla visualizacion terminada

--- ProyectoDeGestionPersonal/Form1.cs
+++ ProyectoDeGestionPersonal/Form1.cs
@@ -19,7 +19,9 @@ namespace ProyectoDeGestionPersonal
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
+            timer1.Start();
+          
         }
 
         private void Form1_Resize(object sender, EventArgs e)
@@ -37,6 +39,11 @@ namespace ProyectoDeGestionPersonal
         private void trackBarSatisfaccion_Scroll(object sender, EventArgs e)
         {
             lblnumeracion.Text = trackBarSatisfaccion.Value.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblFechaHora.Text = DateTime.Now.ToString();
         }
     }
 }
